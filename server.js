4d54051a8cc2317fc3b729d2d4870d1262c815b6
First Last <salmanashraf853@gmail.com>
2019-10-10 22:23:14 +0100

    i

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-let contact = []
-console.log(contact)
+let cont = []
+console.log(cont)
 
 app.get('/', (req, res) =>{
-    res.json(contact)
-    console.log(contact, '........')
+    res.json(cont)
+    console.log(cont, '........')
 })
 
-app.post('/contact-me', (req, res) => {
-    contact.push({
+app.post('/cont-me', (req, res) => {
+    cont.push({
         name: req.body.name,
         email: req.body.email,
         message: req.body.message
     })
-    console.log(contact)
+    console.log(cont)
     .then((data)=>{
         res.json(data)
     }).catch((err)=>{
